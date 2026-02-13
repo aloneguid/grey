b65abf496ad17d10a6b16c0dff9c8d545243e939
aloneguid <aloneguid@outlook.com>
2026-02-13 17:07:30 +0000

    turn off auto-merge for native windows

--- grey/backends/win32dx11app.hpp
+++ grey/backends/win32dx11app.hpp
@@ -365,7 +365,7 @@ namespace grey::backends {
             //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
             //io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // Enable Docking
             io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;       // Enable Multi-Viewport / Platform Windows
-            //io.ConfigViewportsNoAutoMerge = true;
+            io.ConfigViewportsNoAutoMerge = true;
             //io.ConfigViewportsNoTaskBarIcon = true;
             //io.ConfigViewportsNoDefaultParent = true;
             //io.ConfigDockingAlwaysTabBar = true;
