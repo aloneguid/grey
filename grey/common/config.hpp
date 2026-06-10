7bef9c7c30c8d11d3009ce7b8339f3a9d5258897
aloneguid <aloneguid@outlook.com>
2026-06-10 16:38:58 +0100

    imconfig: save color, int, float. Support sections.

--- grey/common/config.hpp
+++ grey/common/config.hpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <filesystem>
 #include <SimpleIni.h> // https://github.com/brofield/simpleini
-
+#include "imgui.h"
 #include "fss.h"
 
 namespace grey::common {
@@ -17,6 +17,7 @@ namespace grey::common {
     class config {
     public:
         using string = std::string;
+        using colour = unsigned int;
 
         config(const std::string& application_name, const std::string& file_name = "config.ini") {
             bool is_portable = std::filesystem::exists(std::filesystem::path{fss::get_current_dir()} / ".portable");
@@ -36,9 +37,12 @@ namespace grey::common {
             reload();
         }
 
-#define X(type, name, default_val) type name{default_val};
+        // declare config members
+#define opt(type, name, default_val) type name{default_val};
+#define opt_sect(type, name, default_val, sect_name, sect_val_name) type name{default_val};
     #include "config.def"
-#undef X
+#undef opt_sect
+#undef opt
 
         ~config() {
             commit();
@@ -50,15 +54,21 @@ namespace grey::common {
             ini.Reset();
             ini.LoadFile(file_path.c_str());
 
-#define X(type, name, default_val) name = get_##type##_value(#name, default_val);
+            // loading logic
+#define opt(type, name, default_val) name = get_##type##_value(#name, default_val);
+#define opt_sect(type, name, default_val, sect_name, sect_val_name) name = get_##type##_value(#sect_val_name, default_val, #sect_name);
     #include "config.def"
-#undef X
+#undef opt_sect
+#undef opt
         }
 
         void commit() {
-#define X(type, name, default_val) set_##type##_value(#name, name);
+            // saving logic
+#define opt(type, name, default_val) set_##type##_value(#name, name);
+#define opt_sect(type, name, default_val, sect_name, sect_val_name) set_##type##_value(#sect_val_name, name, #sect_name);
     #include "config.def"
-#undef X
+#undef opt_sect
+#undef opt
             ini.SaveFile(file_path.c_str());
         }
 
@@ -75,7 +85,23 @@ namespace grey::common {
 
         string get_string_value(const std::string& key, const std::string& default_value, const std::string& section = "") {
             const char* v = ini.GetValue(section.c_str(), key.c_str());
-            return v ? v : "";
+            return v ? v : default_value;
+        }
+
+        void set_int_value(const std::string& key, int value, const std::string& section = "") {
+            ini.SetLongValue(section.c_str(), key.c_str(), value, nullptr, false, true);
+        }
+
+        int get_int_value(const std::string& key, int default_value, const std::string& section = "") {
+            return ini.GetLongValue(section.c_str(), key.c_str(), default_value);
+        }
+
+        void set_float_value(const std::string& key, float value, const std::string& section = "") {
+            ini.SetDoubleValue(section.c_str(), key.c_str(), value, nullptr, true);
+        }
+
+        float get_float_value(const std::string& key, float default_value, const std::string& section = "") {
+            return ini.GetDoubleValue(section.c_str(), key.c_str(), default_value);
         }
 
         void set_bool_value(const std::string& key, bool value, const std::string& section = "") {
@@ -88,5 +114,47 @@ namespace grey::common {
             return value == "true" || value == "y" || value == "1";
         }
 
+        unsigned int get_colour_value(const std::string& key, unsigned int default_value, const std::string& section = "") {
+            string value = get_string_value(key, "", section);
+            if(value.empty()) return default_value;
+            return hex_to_imcol(value);
+        }
+
+        void set_colour_value(const std::string& key, unsigned int value, const std::string& section = "") {
+            set_string_value(key, imcol_to_hex(value), section);
+        }
+
+        unsigned int hex_to_imcol(const std::string& hex) {
+            std::string h = hex;
+
+            // Remove # prefix if present
+            if (!h.empty() && h[0] == '#') {
+                h = h.substr(1);
+            }
+
+            // Parse hex string (expects RRGGBB or RRGGBBAA format)
+            if (h.length() == 6) {
+                try {
+                    unsigned long value = std::stoul(h, nullptr, 16);
+                    // RGB format - add full alpha
+                    float r = ((value >> 16) & 0xFF) / 255.0f;
+                    float g = ((value >> 8) & 0xFF) / 255.0f;
+                    float b = (value & 0xFF) / 255.0f;
+
+                    ImColor ic{r, g, b};
+                    return ic;
+                } catch (...) {
+                    return 0;
+                }
+            }
+
+            return 0;
+        }
+
+        std::string imcol_to_hex(unsigned int color) {
+
+            ImColor ic{color};
+            return std::format("#{:02X}{:02X}{:02X}", (int)(ic.Value.x * 255), (int)(ic.Value.y * 255), (int)(ic.Value.z * 255));
+        }
     };
 }
