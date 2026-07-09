0c41f6c51d12dafcba7cfd1adb3b3d7068b6add1
aloneguid <aloneguid@outlook.com>
2026-07-09 11:32:33 +0100

    state handler improvements

---
+++ grey/common/state_handler.hpp
@@ -0,0 +1,112 @@
+#pragma once
+#include <string>
+#include <filesystem>
+#include <fstream>
+#include <fkYAML/node.hpp>
+#include "fss.h"
+
+namespace grey::common {
+    class state_handler {
+    public:
+        using ynode = fkyaml::node;
+
+        explicit state_handler(const std::string &application_name) {
+            file_path = (std::filesystem::path{grey::common::fss::get_config_dir(application_name)} / "config.yml").
+                    string();
+
+            // create dir if not exists
+            const std::filesystem::path abs{file_path};
+            std::filesystem::create_directories(abs.parent_path());
+        }
+
+        template<typename T>
+        static void read(ynode &n, const std::string &key, T &value, const T default_value) {
+            const ynode &value_node = n[key];
+            value = default_value;
+
+            if constexpr(std::is_same_v<T, std::string>) {
+                if(value_node.is_string())
+                    value = value_node.get_value<std::string>();
+            } else if constexpr(std::is_same_v<T, bool>) {
+                if(value_node.is_boolean())
+                    value = value_node.get_value<bool>();
+            }
+        }
+
+        template<typename T>
+        static void write(ynode &n, const std::string &key, const T &value) {
+            n[key] = value;
+        }
+
+        ynode root;
+
+        void deserialize() {
+            std::ifstream ifs{file_path, std::ios::in};
+            root = fkyaml::node::deserialize(ifs);
+            if(root.get_type() != fkyaml::node_type::MAPPING)
+                root = fkyaml::node::mapping();
+        }
+
+        void serialize() const {
+            std::ofstream ofs{file_path, std::ios::out};
+            ofs << root;
+        }
+
+        std::filesystem::file_time_type get_last_write_time() const {
+            return std::filesystem::last_write_time(file_path);
+        }
+
+    private:
+        std::string file_path;
+    };
+
+    class app_state {
+    public:
+        virtual void serialize() = 0;
+        virtual void deserialize() = 0;
+        virtual std::filesystem::file_time_type get_last_write_time() const = 0;
+        virtual ~app_state() = default;
+    };
+
+    template<typename T>
+    class state_ticker {
+    public:
+        state_ticker(T& state, float flush_interval = 1.0f) : prev_state{state}, state{state}, flush_interval{flush_interval} {
+            last_write_time = state.get_last_write_time();
+        }
+
+        bool tick(float delta_time) {
+            last_flushed_ago += delta_time;
+            bool changed{false};
+
+            if(last_flushed_ago > flush_interval) {
+                // check if our version is old
+                auto new_last_write_time = state.get_last_write_time();
+                if(new_last_write_time > last_write_time) {
+                    state.deserialize();
+                    last_write_time = new_last_write_time;
+                    prev_state = state;
+                    changed = true;
+                } else {
+                    // otherwise check if we need to dump the state
+                    bool are_same = state == prev_state;
+                    if(!are_same) {
+                        state.serialize();
+                        prev_state = state;
+                        last_write_time = state.get_last_write_time();
+                        changed = true;
+                    }
+                }
+                last_flushed_ago = 0.f; // otherwise we'll keep comparing forever
+            }
+
+            return changed;
+        }
+    private:
+        const float flush_interval;
+        T prev_state;   // copy of the state (state needs to support copy constructor
+        T& state;
+        float last_flushed_ago{0.f};
+        std::filesystem::file_time_type last_write_time;
+    };
+}
