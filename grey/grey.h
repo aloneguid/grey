24b20d68bc140dbd21264105e5a0019f23b3c1e8
aloneguid <aloneguid@outlook.com>
2026-07-22 12:18:51 +0100

    icon map

--- grey/grey.h
+++ grey/grey.h
@@ -1,5 +1,10 @@
-// startup includes
-
+#pragma once
+#include <string>
+#include <unordered_map>
 #include "app.h"
 #include "widgets.h"
 #include "fonts/MaterialIcons.h"
+
+const std::unordered_map<std::string, std::string>& get_icon_map();
+
+std::string get_icon(const std::string& icon_name);
